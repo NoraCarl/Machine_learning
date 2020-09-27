640fa8ba98c6e809db5abd7d51b7ff4c4d6983b9
Cora <17623461751@163.com>
2020-09-27 10:37:53 +0800

    回归模型训练任务完成

--- Homework/four.py
+++ Homework/four.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import sklearn
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import LinearRegression
 
 # 解决中文乱码
 plt.rcParams['font.sans-serif']=[u'SimHei']
@@ -30,5 +30,31 @@ for i in range(bt_feature_names.size):
     plt.subplot(5,3,i+1)
     plt.scatter(df_pd_data.iloc[:,i],df_pd_target)
     plt.title(bt_feature_names[i],color='red')
+
+# 统计特征目标值的数量
+print(df_pd_target.count())
+Rlp_date = df_pd_data[['RM','LSTAT','PTRATIO']]
+print(Rlp_date.shape)
+# 绘制图像--绘制画布
+plt.figure(figsize=(16,18))
+for i in range(Rlp_date.columns.size):
+    plt.subplot(2,2,i+1)
+    plt.scatter(Rlp_date.iloc[:,i],df_pd_target)
+    plt.title(Rlp_date.columns[i])
+
+# 规划训练集和测试集 
+x_data, x_test, y_data, y_test = train_test_split(df_pd_data,df_pd_target,test_size=0.2,random_state=5)
+print(x_data.shape,y_data.shape,x_test.shape,y_test.shape)
+# 训练模型 线性回归模型
+Lr = LinearRegression(n_jobs=-1, normalize=True)
+Lr.fit(x_data,y_data)
+Prd = Lr.predict(x_test)
+print('准确率:{}'.format(Lr.score(x_test,y_test)))
+
+# 绘制图像 描述真实值和预测值
+plt.figure(figsize=(14,8))
+plt.plot(range(y_test.shape[0]),y_test,'-')
+plt.plot(range(y_test.shape[0]),Prd,'--')
+plt.legend(('真实值','预测值'),loc='upper right',fontsize=14)
 
 plt.show()
